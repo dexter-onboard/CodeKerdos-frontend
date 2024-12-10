beb0f0803fef5721dc221a81e45b5bd25d6663e1
Sahil Chhabra <74790417+ThisSahil@users.noreply.github.com>
2024-12-10 23:48:52 +0530

    iFrame url changes

--- src/Components/ui/hero-video-dialog.js
+++ src/Components/ui/hero-video-dialog.js
@@ -107,7 +107,7 @@ export default function HeroVideoDialog({
                 <iframe
                   width="900"
                   height="500"
-                  src="https://www.youtube.com/embed/C6hgFHSMkzY?si=6BceuIAkjOJczgT3"
+                  src="https://www.youtube.com/embed/sfiRrl52ngU?si=-iVdQtcyZys0IQdm"
                   title="YouTube video player"
                   frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
