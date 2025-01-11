63d9c643f8bf9709ce20e55d1d74f15f0e23ca16
sudhendra <contactwithsid@gmail.com>
2025-01-12 00:54:29 +0530

    added blog content

--- src/Components/Courses/courseDetailsPdfLinks.js
+++
@@ -1,8 +0,0 @@
-export const mernLink =
-  "https://codekerdos.s3.us-east-1.amazonaws.com/MERN.pdf";
-export const mernSpringLaunchLink =
-  "https://codekerdos.s3.us-east-1.amazonaws.com/Launchpad_Node.js_%26_Springboot.pdf";
-export const mernLaunchLink =
-  "https://codekerdos.s3.us-east-1.amazonaws.com/Launchpad_NodeJS.pdf";
-export const springLaunchLink =
-  "https://codekerdos.s3.us-east-1.amazonaws.com/Launchpad_Spring_boot.pdf";
--- src/utils/userSiteActions.js
+++
@@ -1,6 +0,0 @@
-export function userDataPushActions(userAction, userId = "not needed") {
-  window.dataLayer.push({
-    event: userAction,
-    user_id: userId,
-  });
-}
