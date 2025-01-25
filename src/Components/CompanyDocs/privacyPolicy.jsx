b339c3de82d128cf48307381db7e6a6d5c2b0e40
sudhendra <contactwithsid@gmail.com>
2025-01-25 14:19:59 +0530

    feat/added-privacy-policy-and-tnc

---
+++ src/Components/CompanyDocs/privacyPolicy.jsx
@@ -0,0 +1,232 @@
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <h1>Privacy Policy (Last Updated: November 27, 2024)</h1>
+      <h2>Introduction</h2>
+      This Privacy Policy governs the collection, use, and disclosure of
+      personal information, including sensitive or special categories of data,
+      by Kerdos Coding LLP (“Kerdos Coding”, “Code Kerdos”, “we”, or “our”).
+      "Personal Data" refers to information that can identify an individual
+      (“you” or “your”), as outlined below. Kerdos Coding LLP is committed to
+      safeguarding your privacy and ensuring a secure experience using our
+      website, products, and services (collectively called “Products”). This
+      policy applies to the Kerdos Coding LLP website and all associated
+      subdomains. Please refer to the following link for our Terms of Service.
+      This document explains how Kerdos Coding LLP, along with its subsidiaries
+      and affiliated entities, handles your data and outlines the practices we
+      follow to collect and use the information you provide when accessing our
+      systems through online platforms (e.g., website, mobile apps)
+      (collectively referred to as “Company Systems”). In this policy, "Personal
+      Data" refers to any information that can be used to identify an
+      individual, such as your name, email address, postal address, phone
+      number, or other personally identifiable details. By using our Products,
+      you acknowledge that you have read, understood, and agreed to the terms of
+      this policy. Unless otherwise stated, Kerdos Coding LLP will be the
+      processor of any Personal Data provided, collected, or processed by this
+      policy.
+      <h2>Scope of This Privacy Policy</h2>
+      This Privacy Policy applies to all information we collect through the
+      Company Systems, including any services provided by Kerdos Coding LLP.
+      However, it does not cover any data collected by third parties, whether
+      through our Company Systems, external links, advertisements, or automated
+      tools (such as bots). Kerdos Coding is not responsible for third-party
+      data collection practices.
+      <h2>Information We Collect</h2>
+      We collect information that you choose to share with us. You have control
+      over what information you provide, and we will only collect data that you
+      actively submit via our Company Systems. The types of information we
+      collect include:
+      <b>Personal Information (PII):</b> This includes your name, title, gender,
+      date of birth, email address, phone numbers (home, work, or mobile),
+      social media handles (e.g., Facebook, Google, GitHub), postal address, IP
+      address, and any other personal details you may submit during registration
+      or usage of the services.
+      <b>Account Information:</b> Details necessary for registration and
+      continued use of the Company Systems, including login credentials, profile
+      information, resume/CV, social media links, school/university details
+      (e.g., grades, graduation year), workplace information (e.g., job title,
+      salary), and other identifiers.
+      <b>Additional Information:</b> You may also choose to provide us with
+      additional information, such as updates to your account, responses to
+      surveys, or participation in discussions, articles, and other features
+      within the Company Systems.
+      <h2>Purpose of Data Collection</h2>
+      We collect and process your data for the following purposes:
+      <b>Account Registration and Product Features:</b> To facilitate account
+      creation, manage your use of Product features, generate reports, and
+      create or take tests.
+      <b>Service Requests and Support:</b> To provide assistance and support,
+      register for events, or participate in surveys and discussions.
+      <b>Content Customization and Subscription:</b> To tailor content to your
+      preferences, register for newsletters, and customize the content you see.
+      <h2>Sensitive Data</h2>
+      Please avoid sharing sensitive personal information (such as financial
+      data, health details, etc.) unless specifically requested by Kerdos Coding
+      LLP for registration or related processes. If any content you provide
+      violates this policy, Kerdos Coding LLP reserves the right to remove such
+      content at its discretion.
+      <h2>Third-Party Data Sharing</h2>
+      We may also collect or receive information from third-party sources (e.g.,
+      business partners and affiliated entities). For example, if you log into
+      our Company Systems via a third-party platform like Facebook, LinkedIn, or
+      Twitter, we may combine the information we receive from these platforms
+      with the data you’ve submitted to our systems, creating a more
+      comprehensive profile for you.
+      <h2>Use of Your Personal Information</h2>
+      We process your Personal Data only by this Privacy Policy and solely for
+      the purposes outlined here. To fulfill our contractual obligations to you
+      and provide the services you’ve requested, we collect and process your
+      Personal Data for the following activities: Account Creation and
+      Management: To register and maintain your account on our platform.
+      Platform Usage: To facilitate the use of our website and services,
+      including taking tests and generating reports. Customer Support: To offer
+      service and support related to our products. Event Registration and
+      Participation: To allow participation in hackathons, surveys, and other
+      events. Customization and Personalization: To tailor content to your
+      preferences and interests. Job Referrals: To recommend your profile to
+      relevant job opportunities, with your explicit consent.
+      <h2>Additional Purposes of Data Processing</h2>
+      Beyond the basic services mentioned above, we may use your data for:
+      Sending you updates about new product features, promotions, and special
+      offers. Providing support and assistance for our services. Creating and
+      managing your user profile to enhance your experience. Communicating with
+      you regarding your account or customer service needs. Offering
+      personalized content and marketing based on your interests and activity on
+      our platform. Collecting feedback through surveys or questionnaires to
+      improve our products and services. Supporting recruitment inquiries and
+      offering job opportunities based on your profile. Meeting legal
+      obligations or contractual requirements, where applicable. We may share
+      your profile with employers in an anonymized form and, with your consent,
+      allow them to contact you for job opportunities. If you wish to opt out of
+      this feature, you may email us at support@codekerdos.in
+      <h2>Consent to Process Your Data</h2>
+      By registering and creating an account on our platform, you voluntarily
+      provide your consent for us to process your Data as outlined in this
+      Privacy Policy. If you choose not to provide certain Personal Data, you
+      may not be able to access specific features or services. You may also
+      subscribe to receive communications such as emails or newsletters from us.
+      Should you wish to unsubscribe from these communications, you can update
+      your preferences by using the ‘Unsubscribe’ option or by contacting us
+      directly at support@codekerdos.in
+      <h2>How We Use Your Information</h2>
+      We will only process your Data when we have a valid legal basis, including
+      but not limited to the following reasons: Consent: If you explicitly
+      provide us with your information. Contractual Necessity: To perform our
+      agreement with you, such as providing access to our products or services.
+      Legitimate Interests: Our legitimate interest in providing, improving, or
+      securing our services, or for reasons such as fraud detection, marketing,
+      or legal obligations. We may use your Data to: Provide the services you’ve
+      requested, including sending you announcements or updates related to your
+      usage. Respond to your inquiries and resolve any issues. Diagnose and
+      address technical problems with our platform. Send you marketing materials
+      or administrative notices. Monitor how our services are being used to
+      enhance the platform. Prevent fraudulent activity or violations of our
+      terms. Comply with legal and regulatory requirements and resolve disputes.
+      If you wish to stop us from processing your Personal Data, you can
+      deactivate your account through the account settings at any time.
+      <h2>How We Organize and Store Your Data</h2>
+      We utilize various technologies and systems to store and manage your Data
+      securely. Below is an overview of the platforms and services we use: AWS
+      Relational Database: The primary database used to store all user-related
+      information. ElasticSearch: An indexing system for user resumes, enabling
+      us to query and search the content of resumes effectively. Sentry: A
+      service used for monitoring errors in the system and ensuring smooth
+      operation. New Relic: Stores server logs, which help us optimize our
+      services and monitor performance. Amazon S3: Used for storing server logs
+      and user resume files. Google Analytics: A third-party service that allows
+      us to monitor and analyze website and service traffic. Google Analytics
+      collects data via cookies and usage data, which helps us enhance user
+      experience.
+      <h2>Your Rights Regarding Personal Data</h2>
+      You have several rights concerning the Personal Data we collect. These
+      rights allow you to manage how your data is handled and processed: Access:
+      You may request information about the Personal Data we hold about you, and
+      you can request a copy of it. Correction: If you believe any of your
+      Personal Data is inaccurate or incomplete, you can request corrections or
+      updates. You can also directly correct some of this information through
+      your user profile settings. Objection: You have the right to object to the
+      processing of your Personal Data for specific purposes. Erasure: You may
+      request the deletion of some or all of your Personal Data. We will comply
+      with this request, and while the data will be deleted immediately,
+      residual data may be retained in certain logs for up to one year.
+      Restriction: You can request the deactivation of your account to restrict
+      further processing of your Personal Data. Portability: You have the right
+      to obtain a copy of your Personal Data in a machine-readable format.
+      Withdrawal of Consent: If we process your Personal Data based on consent,
+      you have the right to withdraw it at any time. Please note that
+      withdrawing consent may affect your ability to use some services.
+      <h2>Data Retention</h2>
+      We retain your Data as long as you have an active account with Kerdos
+      Coding LLP. If you request the deletion of your data, it will be processed
+      within thirty (30) days. However, residual data may be stored in logs,
+      which will be purged within a year of the deletion. Once your data is
+      deleted, your Kerdos Coding account will be deactivated. For any data or
+      account deletion requests, please contact us at support@codekerdos.in
+      <h2>Passive Data Collection</h2>
+      Kerdos Coding LLP may automatically collect certain data when you use our
+      Products, including cookies and other tracking technologies. Information
+      automatically collected includes: Cookies and usage data IP addresses,
+      geolocation, browser, and OS details Page views, files viewed/downloaded,
+      referral and exit pages, and clickstream data This data is stored in logs
+      or third-party analytics tools like Google Analytics and LinkedIn
+      Insights. We do not delete this data unless required.
+      <h2>Cookies and Tracking Technologies</h2>
+      We use cookies to enhance your experience by remembering your preferences
+      and helping us analyze site traffic. Cookies also allow us to track user
+      activity and optimize the website. You can choose to disable cookies via
+      your browser settings, but this may affect some features and services on
+      the site.
+      <h2>Sharing Your Data</h2>
+      General Sharing: We may share your data with our employees, agents,
+      subsidiaries, and third-party service providers to operate the Company
+      Systems, provide customer support, and comply with legal obligations.
+      Hiring Partners: With your consent, we may share your data with hiring
+      partners to facilitate job opportunities. Reorganization Events: In cases
+      of mergers, acquisitions, or bankruptcy, your information may be
+      transferred or shared as part of those processes. We will never sell or
+      rent your data to third parties for marketing purposes without your
+      explicit consent.
+      <h2>Data Security</h2>
+      We protect your data using secure servers, firewalls, and vulnerability
+      scanning. We also use SSL encryption to safeguard data during
+      transmission. However, we cannot guarantee that our security measures are
+      completely foolproof, and you are responsible for keeping your login
+      details secure. |<h2>Third-party websites and Ad Networks</h2>
+      External Websites: We may provide links to external sites for convenience.
+      However, we do not endorse or take responsibility for their content or
+      security. Ad Networks: Third-party ad networks may use cookies and other
+      tracking technologies to display personalized ads based on your
+      interactions with our website. These networks are governed by their
+      privacy policies.
+      <h2>Anonymized Data</h2>
+      Kerdos Coding LLP may use anonymous and aggregated data for reports,
+      statistical analysis, and benchmarking purposes. This data will not
+      identify you personally.
+      <h2>Age Restrictions</h2>
+      You confirm that you are at least 18 years old, or that you have a parent
+      or legal guardian's consent to use our services. We will delete any data
+      from users under 18 if we find that it was submitted in violation of this
+      policy.
+      <h2>Policy Updates</h2>
+      We may update this privacy policy from time to time. We recommend that you
+      review it regularly for any changes.
+      <h2>Grievance Redressal</h2>
+      If you have any concerns or grievances related to this policy, or how we
+      collect, store, or use your personal information, please reach out to us
+      at support@codekerdos.in. We are committed to addressing your grievance
+      and will make every reasonable effort to resolve it within 30 days of
+      receiving your complaint.
+      <h2>Contact Us</h2>
+      For any questions about this policy or any other concerns regarding our
+      services, please contact us at support@codekerdos.in. For security-related
+      inquiries or concerns, please reach out to us at support@codekerdos.in.
+      This privacy policy should be read alongside any other agreements you may
+      have entered into with Kerdos Coding LLP and the Terms of Service (ToS)
+      available on the Codekerdos.in website. By using our services, you consent
+      to Kerdos Coding LLP's use and disclosure of your personal information as
+      outlined in this policy.
+    </>
+  );
+};
+
+export default PrivacyPolicy;
